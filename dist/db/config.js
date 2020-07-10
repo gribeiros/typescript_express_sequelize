0aac7b3cbfdf19605269f434e602fc017e3bb11d
gribeiros <gustavors1998@gmail.com>
2020-07-10 17:34:03 -0300

    add-config-database

---
+++ dist/db/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "root",
+    "database": "ts-api",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+
+  },
+  "test": {
+    "username": "postgres",
+    "password": "root",
+    "database": "ts-api-test",
+    "host": "127.0.0.1",
+    "dialect": "postgres",
+    "logging": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
